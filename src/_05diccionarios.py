2ca93edfadc7d583472171a143b8a2eb56849770
Sergi-Virgili <svirgilif@gmail.com>
2023-12-02 19:34:01 +0100

    dictionary functions

--- src/_05diccionarios.py
+++ src/_05diccionarios.py
@@ -4,29 +4,25 @@ def agregar_par_clave_valor(diccionario, clave, valor):
     """
     Agrega un par clave-valor al diccionario.
     """
-    diccionario[clave] = valor
-    return diccionario
+    pass
 
 def eliminar_par_clave_valor(diccionario, clave):
     """
     Elimina el par clave-valor del diccionario usando la clave.
     Si la clave no existe, devuelve el diccionario sin cambios.
     """
-    diccionario.pop(clave, None)
-    return diccionario
+    pass
 
 def obtener_valor(diccionario, clave):
     """
     Devuelve el valor asociado con la clave en el diccionario.
     Si la clave no existe, devuelve None.
     """
-    return diccionario.get(clave)
+    pass
 
 def combinar_diccionarios(diccionario1, diccionario2):
     """
     Combina dos diccionarios en uno nuevo y lo devuelve.
     Si hay claves duplicadas, los valores de diccionario2 prevalecerán.
     """
-    nuevo_diccionario = diccionario1.copy()
-    nuevo_diccionario.update(diccionario2)
-    return nuevo_diccionario
+    pass
